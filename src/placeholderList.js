7d4783ad681a311092fef80ed442e7cc94acf12c
Wendi Zhang <wendizhang68@gmail.com>
2022-08-18 21:46:07 -0400

    updated placeholder

---
+++ src/placeholderList.js
@@ -0,0 +1,13 @@
+const placeholderList = [
+  {track:{track_id: 225609709, track_name: "Run Away to Mars",artist_name: "Talk",
+  primary_genres: {music_genre_list: [{music_genre:{music_genre_id: 21,
+    music_genre_name: "Rock"}}]},track_share_url: "https://www.musixmatch.com/lyrics/Talk/Run-Away-to-Mars?utm_…ation&utm_campaign=api&utm_medium=Hacker+You%3A1409622707534"}},
+  {
+
+  },
+  {
+
+  }
+  
+
+]
