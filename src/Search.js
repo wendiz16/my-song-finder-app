408ff5d432912204ab590a77f3da9e2308510557
Wendi Zhang <wendizhang68@gmail.com>
2022-09-06 14:40:33 -0400

    updated countries in form

--- src/Search.js
+++ src/Search.js
@@ -15,17 +15,18 @@ function Search(props){
           <select value={userChoice}
           onChange={handleUserChoice} name="countries" id="countries">
             <option value="placeholder" disabled>Pick Country here:</option>
-            <option value="ca">Canada</option>
-            <option value="us">USA</option>
-            <option value="uk">UK</option>
-            <option value="it">Italy</option>
-            <option value="fr">France</option>
-            <option value="sg">Singapore</option>
-            <option value="de">Germany</option>
-            <option value="jp">Japan</option>
-            <option value="in">India</option>
             <option value="au">Australia</option>
-          
+            <option value="br">Brazil</option>
+            <option value="ca">Canada</option>
+            <option value="fr">France</option>
+            <option value="de">Germany</option>
+            <option value="in">India</option>
+            <option value="it">Italy</option>
+            <option value="jp">Japan</option>
+            <option value="sg">Singapore</option>
+            <option value="za">South Africa</option>
+            <option value="uk">UK</option>
+            <option value="us">USA</option>
           </select>
           <button type="submit">click me to get songs!</button>
         </form>
