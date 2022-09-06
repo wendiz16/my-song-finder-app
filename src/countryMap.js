a97479911df82bc524d69e737667c49c9df99219
Wendi Zhang <wendizhang68@gmail.com>
2022-09-06 14:35:01 -0400

    updated countryMap

--- src/countryMap.js
+++ src/countryMap.js
@@ -1,13 +1,15 @@
 const countryMap = new Map();
-    countryMap.set("ca","Canada");
-    countryMap.set("us","USA");
-    countryMap.set("uk","UK");
-    countryMap.set("it","Italy");
-    countryMap.set("fr","France");
-    countryMap.set("sg","Singapore");
-    countryMap.set("de","Germany");
-    countryMap.set("jp","Japan");
-    countryMap.set("in","India");
-    countryMap.set("au","Australia");
+ countryMap.set("au","Australia");
+ countryMap.set("br","Brazil");
+ countryMap.set("ca","Canada");
+ countryMap.set("fr","France");
+ countryMap.set("de","Germany");
+ countryMap.set("in","India");
+ countryMap.set("it","Italy");
+ countryMap.set("jp","Japan");
+ countryMap.set("sg","Singapore");
+ countryMap.set("za","South Africa");
+ countryMap.set("uk","UK");
+ countryMap.set("us","USA");
 
   export default countryMap;
